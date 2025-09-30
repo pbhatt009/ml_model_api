92b908bdda13139b958471f68b5b572e1b0763a3
pbhatt009 <bhattpriyanshu49@gmail.com>
2025-09-30 21:44:16 +0530

    Update .gitignore, fix Procfile port, enhance TextPreprocess error handling, and refine requirements.txt

--- complaint_classifier/Text_preprocess.py
+++ complaint_classifier/Text_preprocess.py
@@ -5,9 +5,12 @@ import re
 import string
 from textblob import TextBlob
 import spacy
-
-
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
 
 
 
